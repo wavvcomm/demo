09030790ebaa4f01694d0a195acb7b59872cd087
clintkunz <clint.kunz@stormapp.com>
2021-01-20 14:55:10 -0700

    add firstName and lastName, update onContactLink and onContactSearch

--- src/DetailView.js
+++ src/DetailView.js
@@ -9,233 +9,235 @@ import { store } from './store';
 import { SET_OPEN_NOTE } from './types';
 
 const DetailView = ({ match, getContactById }) => {
-	const {
-		notes,
-		outcomes,
-		unreadCounts,
-		authed,
-		tags,
-		enableClickToCall,
-		numberDialing,
-		dispatch,
-		dncList,
-	} = useContext(store);
-	const [activeMain, setActiveMain] = useState('notes');
-	const [note, setNote] = useState('');
-	const { id } = match.params;
-	const contact = getContactById(id);
+  const {
+    notes,
+    outcomes,
+    unreadCounts,
+    authed,
+    tags,
+    enableClickToCall,
+    numberDialing,
+    dispatch,
+    dncList,
+  } = useContext(store);
+  const [activeMain, setActiveMain] = useState('notes');
+  const [note, setNote] = useState('');
+  const { id } = match.params;
+  const contact = getContactById(id);
 
-	useEffect(() => {
-		const waitingForContinueListener = addWaitingForContinueListener(({ waiting }) => {
-			if (waiting) dispatch({ type: SET_OPEN_NOTE, payload: true });
-		});
-		return () => {
-			waitingForContinueListener.remove();
-		};
-	}, []);
+  useEffect(() => {
+    const waitingForContinueListener = addWaitingForContinueListener(({ waiting }) => {
+      if (waiting) dispatch({ type: SET_OPEN_NOTE, payload: true });
+    });
+    return () => {
+      waitingForContinueListener.remove();
+    };
+  }, []);
 
-	return (
-		<Container>
-			<HeaderContainer>
-				<Grid>
-					<Grid.Column width={3} style={{ minWidth: 175 }}>
-						<Image
-							size="medium"
-							src={
-								contact?.avatarUrl ||
-								'https://res.cloudinary.com/stormapp/image/upload/v1567524915/avatar_uwqncn.png'
-							}
-							className="profilePicture"
-						/>
-					</Grid.Column>
-					<Grid.Column width={10}>
-						<Header as="h3" className="contactName">
-							{contact.name}
-						</Header>
-						<List>
-							{contact.address && contact.city && (
-								<List.Item
-									className="contactAddress"
-									icon="marker"
-									content={`${contact.address} ${contact.city}`}
-								/>
-							)}
-							{contact.numbers.map((number) => {
-								const dncNumber = !!dncList[rawPhone(number)];
-								const removable = dncList[rawPhone(number)]?.removable;
-								return (
-									<Number
-										key={number}
-										dialing={formatPhone(numberDialing) === formatPhone(number)}
-										dncNumber={dncNumber}
-									>
-										{formatPhone(number)}
-										<Popup
-											content="Call Number"
-											position="bottom center"
-											trigger={
-												<Button
-													icon="phone"
-													size="mini"
-													style={{ margin: '3px 3px 3px 6px' }}
-													disabled={!enableClickToCall || !authed}
-													onClick={() => callPhone({ number })}
-												/>
-											}
-										/>
+  if (!contact) return <div>No contact found with that id</div>;
 
-										<Popup
-											content="Message Number"
-											position="bottom center"
-											trigger={
-												<div style={{ position: 'relative' }}>
-													<Button
-														icon="comment alternate"
-														size="mini"
-														style={{ margin: 3 }}
-														disabled={!authed}
-														onClick={() =>
-															openMessengerThread({ contact, number, contactView: true })
-														}
-													/>
-													{unreadCounts[number] ? (
-														<Label
-															color="red"
-															size="tiny"
-															circular
-															floating
-															content={unreadCounts[number]}
-														/>
-													) : null}
-												</div>
-											}
-										/>
-										<Popup
-											content={dncNumber ? 'Remove from Do Not Call' : 'Do Not Call'}
-											position="bottom center"
-											trigger={
-												<Button
-													onClick={() => {
-														const methodName = dncNumber
-															? 'removeDncNumber'
-															: 'addDncNumber';
-														const dncFunc = dncNumber ? removeDncNumber : addDncNumber;
-														dncFunc({ number })
-															.then(() => debugLogger({ name: methodName, dispatch }))
-															.catch(() =>
-																debugLogger({ name: `${methodName} failed`, dispatch })
-															);
-													}}
-													disabled={dncNumber && !removable}
-													icon="exclamation triangle"
-													size="mini"
-													color={dncNumber ? 'red' : null}
-													style={{ margin: 3 }}
-												/>
-											}
-										/>
-									</Number>
-								);
-							})}
-						</List>
-						<div>
-							{tags?.[id]?.['Warm Lead'] && (
-								<Label as="h6" tag color="blue" style={{ marginRight: 7 }}>
-									Warm Lead
-								</Label>
-							)}
-							{tags?.[id]?.['Follow Up'] && (
-								<Label as="h6" tag color="teal" style={{ marginRight: 7 }}>
-									Follow Up
-								</Label>
-							)}
-							{tags?.[id]?.['Do Not Call'] && (
-								<Label as="h6" tag color="red">
-									Do Not Call
-								</Label>
-							)}
-						</div>
-					</Grid.Column>
-				</Grid>
-			</HeaderContainer>
-			<MainContainer>
-				<Menu secondary>
-					<Menu.Item
-						name="Notes"
-						active={activeMain === 'notes'}
-						onClick={() => {
-							setActiveMain('notes');
-						}}
-					/>
-					<Menu.Item
-						name="Call Outcomes"
-						active={activeMain === 'callOutcomes'}
-						onClick={() => {
-							setActiveMain('callOutcomes');
-						}}
-					/>
-				</Menu>
-				{activeMain === 'notes' ? (
-					<FeedContainer>
-						<Feed>
-							{notes[id]?.map(({ note: nte, date }) => (
-								<Feed.Event key={date} style={{ marginBottom: 20 }}>
-									<Feed.Content>
-										<Feed.Summary>{new Date(date).toLocaleDateString('en-US')}</Feed.Summary>
-										<Feed.Extra>{nte}</Feed.Extra>
-									</Feed.Content>
-								</Feed.Event>
-							))}
-						</Feed>
-					</FeedContainer>
-				) : (
-					<FeedContainer>
-						<Feed>
-							{outcomes[id]?.map(({ number, duration, human, outcome, date }) => (
-								<Feed.Event key={`${number} - ${duration} - ${outcome}`} style={{ marginBottom: 15 }}>
-									<Feed.Content>
-										<Feed.Summary>{new Date(date).toLocaleDateString('en-US')}</Feed.Summary>
-										<Feed.Extra>
-											{outcome} {human ? '- human answered' : ''}
-										</Feed.Extra>
-									</Feed.Content>
-								</Feed.Event>
-							))}
-						</Feed>
-					</FeedContainer>
-				)}
-			</MainContainer>
-			<CallDispositionModal contactId={id} note={note} setNote={setNote} />
-		</Container>
-	);
+  return (
+    <Container>
+      <HeaderContainer>
+        <Grid>
+          <Grid.Column width={3} style={{ minWidth: 175 }}>
+            <Image
+              size="medium"
+              src={
+                contact?.avatarUrl ||
+                'https://res.cloudinary.com/stormapp/image/upload/v1567524915/avatar_uwqncn.png'
+              }
+              className="profilePicture"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Header as="h3" className="contactName">
+              {contact.name}
+            </Header>
+            <List>
+              {contact.address && contact.city && (
+                <List.Item
+                  className="contactAddress"
+                  icon="marker"
+                  content={`${contact.address} ${contact.city}`}
+                />
+              )}
+              {contact.numbers.map((number) => {
+                const dncNumber = !!dncList[rawPhone(number)];
+                const removable = dncList[rawPhone(number)]?.removable;
+                return (
+                  <Number
+                    key={number}
+                    dialing={formatPhone(numberDialing) === formatPhone(number)}
+                    dncNumber={dncNumber}
+                  >
+                    {formatPhone(number)}
+                    <Popup
+                      content="Call Number"
+                      position="bottom center"
+                      trigger={
+                        <Button
+                          icon="phone"
+                          size="mini"
+                          style={{ margin: '3px 3px 3px 6px' }}
+                          disabled={!enableClickToCall || !authed}
+                          onClick={() => callPhone({ number })}
+                        />
+                      }
+                    />
+
+                    <Popup
+                      content="Message Number"
+                      position="bottom center"
+                      trigger={
+                        <div style={{ position: 'relative' }}>
+                          <Button
+                            icon="comment alternate"
+                            size="mini"
+                            style={{ margin: 3 }}
+                            disabled={!authed}
+                            onClick={() =>
+                              openMessengerThread({ contact, number, contactView: true })
+                            }
+                          />
+                          {unreadCounts[number] ? (
+                            <Label
+                              color="red"
+                              size="tiny"
+                              circular
+                              floating
+                              content={unreadCounts[number]}
+                            />
+                          ) : null}
+                        </div>
+                      }
+                    />
+                    <Popup
+                      content={dncNumber ? 'Remove from Do Not Call' : 'Do Not Call'}
+                      position="bottom center"
+                      trigger={
+                        <Button
+                          onClick={() => {
+                            const methodName = dncNumber
+                              ? 'removeDncNumber'
+                              : 'addDncNumber';
+                            const dncFunc = dncNumber ? removeDncNumber : addDncNumber;
+                            dncFunc({ number })
+                              .then(() => debugLogger({ name: methodName, dispatch }))
+                              .catch(() =>
+                                debugLogger({ name: `${methodName} failed`, dispatch })
+                              );
+                          }}
+                          disabled={dncNumber && !removable}
+                          icon="exclamation triangle"
+                          size="mini"
+                          color={dncNumber ? 'red' : null}
+                          style={{ margin: 3 }}
+                        />
+                      }
+                    />
+                  </Number>
+                );
+              })}
+            </List>
+            <div>
+              {tags?.[id]?.['Warm Lead'] && (
+                <Label as="h6" tag color="blue" style={{ marginRight: 7 }}>
+                  Warm Lead
+                </Label>
+              )}
+              {tags?.[id]?.['Follow Up'] && (
+                <Label as="h6" tag color="teal" style={{ marginRight: 7 }}>
+                  Follow Up
+                </Label>
+              )}
+              {tags?.[id]?.['Do Not Call'] && (
+                <Label as="h6" tag color="red">
+                  Do Not Call
+                </Label>
+              )}
+            </div>
+          </Grid.Column>
+        </Grid>
+      </HeaderContainer>
+      <MainContainer>
+        <Menu secondary>
+          <Menu.Item
+            name="Notes"
+            active={activeMain === 'notes'}
+            onClick={() => {
+              setActiveMain('notes');
+            }}
+          />
+          <Menu.Item
+            name="Call Outcomes"
+            active={activeMain === 'callOutcomes'}
+            onClick={() => {
+              setActiveMain('callOutcomes');
+            }}
+          />
+        </Menu>
+        {activeMain === 'notes' ? (
+          <FeedContainer>
+            <Feed>
+              {notes[id]?.map(({ note: nte, date }) => (
+                <Feed.Event key={date} style={{ marginBottom: 20 }}>
+                  <Feed.Content>
+                    <Feed.Summary>{new Date(date).toLocaleDateString('en-US')}</Feed.Summary>
+                    <Feed.Extra>{nte}</Feed.Extra>
+                  </Feed.Content>
+                </Feed.Event>
+              ))}
+            </Feed>
+          </FeedContainer>
+        ) : (
+            <FeedContainer>
+              <Feed>
+                {outcomes[id]?.map(({ number, duration, human, outcome, date }) => (
+                  <Feed.Event key={`${number} - ${duration} - ${outcome}`} style={{ marginBottom: 15 }}>
+                    <Feed.Content>
+                      <Feed.Summary>{new Date(date).toLocaleDateString('en-US')}</Feed.Summary>
+                      <Feed.Extra>
+                        {outcome} {human ? '- human answered' : ''}
+                      </Feed.Extra>
+                    </Feed.Content>
+                  </Feed.Event>
+                ))}
+              </Feed>
+            </FeedContainer>
+          )}
+      </MainContainer>
+      <CallDispositionModal contactId={id} note={note} setNote={setNote} />
+    </Container>
+  );
 };
 
 const Container = styled.div({});
 
 const HeaderContainer = styled.div({
-	borderBottom: '1px solid #EAEAEA',
-	padding: 20,
+  borderBottom: '1px solid #EAEAEA',
+  padding: 20,
 });
 
 const FeedContainer = styled.div({
-	padding: '5px 10px',
-	width: '75%',
+  padding: '5px 10px',
+  width: '75%',
 });
 
 const MainContainer = styled.div({
-	padding: 20,
-	marginTop: 20,
+  padding: 20,
+  marginTop: 20,
 });
 
 const Number = styled.div(({ dialing, dncNumber }) => ({
-	display: 'flex',
-	width: 'fit-content',
-	alignItems: 'center',
-	marginTop: 12,
-	paddingLeft: 4,
-	border: dialing && '1px solid #2185D0',
-	borderRadius: 5,
-	textDecoration: dncNumber && 'line-through',
+  display: 'flex',
+  width: 'fit-content',
+  alignItems: 'center',
+  marginTop: 12,
+  paddingLeft: 4,
+  border: dialing && '1px solid #2185D0',
+  borderRadius: 5,
+  textDecoration: dncNumber && 'line-through',
 }));
 
 export default DetailView;
--- src/constants.js
+++ src/constants.js
@@ -1,99 +1,99 @@
 export const contacts = [
-	{
-		contactId: '1',
-		numbers: ['9588880000'],
-		address: '123 E. 456 S.',
-		city: 'Some City',
-		name: 'Victor Voicemail',
-		avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635973/2_bt1i2a.png',
-		mergeFields: [
-			{ id: 'first_name', label: 'First Name', value: 'Victor' },
-			{ id: 'last_name', label: 'Last Name', value: 'Voicemail' },
-			{ id: 'email', label: 'Email', value: 'test@test.com' },
-		],
-	},
-	{
-		contactId: '3',
-		numbers: ['9586660000', '9586660001', '9586660002'],
-		address: '123 E. 456 S.',
-		city: 'Some City',
-		name: 'Rachel Ring',
-		avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635949/3_rqjcbi.png',
-		mergeFields: [
-			{ id: 'first_name', label: 'First Name', value: 'Rachel' },
-			{ id: 'last_name', label: 'Last Name', value: 'Ring' },
-			{ id: 'email', label: 'Email', value: 'test@test.com' },
-		],
-	},
-	{
-		contactId: '2',
-		numbers: ['9587770000', '9587770001'],
-		address: '123 E. 456 S.',
-		city: 'Some City',
-		name: 'Alice Answer',
-		avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635993/1_mfsioi.png',
-		mergeFields: [
-			{ id: 'first_name', label: 'First Name', value: 'Alice' },
-			{ id: 'last_name', label: 'Last Name', value: 'Answer' },
-			{ id: 'email', label: 'Email', value: 'test@test.com' },
-		],
-	},
-	{
-		contactId: '4',
-		numbers: ['9585550000'],
-		address: '123 E. 456 S.',
-		city: 'Some City',
-		name: 'Eric Echo',
-		avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605636272/crop-2020-11-17_1_awpief.jpg',
-		mergeFields: [
-			{ id: 'first_name', label: 'First Name', value: 'Eric' },
-			{ id: 'last_name', label: 'Last Name', value: 'Echo' },
-			{ id: 'email', label: 'Email', value: 'test@test.com' },
-		],
-	},
-	{
-		contactId: '5',
-		numbers: ['9584440000', '9584440001'],
-		address: '123 E. 456 S.',
-		city: 'Some City',
-		name: 'Hank Hangup',
-		avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605636306/crop-2020-11-17_chpw7v.jpg',
-		mergeFields: [
-			{ id: 'first_name', label: 'First Name', value: 'Hank' },
-			{ id: 'last_name', label: 'Last Name', value: 'Hangup' },
-			{ id: 'email', label: 'Email', value: 'test@test.com' },
-		],
-	},
+  {
+    contactId: '1',
+    numbers: ['9588880000'],
+    address: '123 E. 456 S.',
+    city: 'Some City',
+    name: 'Victor Voicemail',
+    firstName: 'Victor',
+    lastName: 'Voicemail',
+    avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635973/2_bt1i2a.png',
+    mergeFields: [
+      { id: 'email', label: 'Email', value: 'victor@test.com' },
+    ],
+  },
+  {
+    contactId: '3',
+    numbers: ['9586660000', '9586660001', '9586660002'],
+    address: '123 E. 456 S.',
+    city: 'Some City',
+    name: 'Rachel Ring',
+    firstName: 'Rachel',
+    lastName: 'Ring',
+    avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635949/3_rqjcbi.png',
+    mergeFields: [
+      { id: 'email', label: 'Email', value: 'rachel@test.com' },
+    ],
+  },
+  {
+    contactId: '2',
+    numbers: ['9587770000', '9587770001'],
+    address: '123 E. 456 S.',
+    city: 'Some City',
+    name: 'Alice Answer',
+    firstName: 'Alice',
+    lastName: 'Answer',
+    avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605635993/1_mfsioi.png',
+    mergeFields: [
+      { id: 'email', label: 'Email', value: 'alice@test.com' },
+    ],
+  },
+  {
+    contactId: '4',
+    numbers: ['9585550000'],
+    address: '123 E. 456 S.',
+    city: 'Some City',
+    name: 'Eric Echo',
+    firstName: 'Eric',
+    lastName: 'Echo',
+    avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605636272/crop-2020-11-17_1_awpief.jpg',
+    mergeFields: [
+      { id: 'email', label: 'Email', value: 'eric@test.com' },
+    ],
+  },
+  {
+    contactId: '5',
+    numbers: ['9584440000', '9584440001'],
+    address: '123 E. 456 S.',
+    city: 'Some City',
+    name: 'Hank Hangup',
+    firstName: 'Hank',
+    lastName: 'Hangup',
+    avatarUrl: 'https://res.cloudinary.com/stormapp/image/upload/v1605636306/crop-2020-11-17_chpw7v.jpg',
+    mergeFields: [
+      { id: 'email', label: 'Email', value: 'hank@test.com' },
+    ],
+  },
 ];
 
 export const exampleNotes = [
-	{
-		note:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. *See recording',
-		date: '2019-02-19T06:00:00Z',
-		number: '2029659970',
-	},
-	{
-		note:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		date: '2019-05-14T06:00:00Z',
-		number: '2029659970',
-	},
+  {
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. *See recording',
+    date: '2019-02-19T06:00:00Z',
+    number: '2029659970',
+  },
+  {
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    date: '2019-05-14T06:00:00Z',
+    number: '2029659970',
+  },
 ];
 
 export const exampleOutcomes = [
-	{
-		date: '2020-10-13T06:00:00Z',
-		number: '2029659970',
-		duration: 32,
-		outcome: 'USER_HUNG_UP',
-		human: true,
-	},
-	{
-		date: '2020-07-03T06:00:00Z',
-		number: '2029659970',
-		duration: 7,
-		outcome: 'BUSY',
-		human: false,
-	},
+  {
+    date: '2020-10-13T06:00:00Z',
+    number: '2029659970',
+    duration: 32,
+    outcome: 'USER_HUNG_UP',
+    human: true,
+  },
+  {
+    date: '2020-07-03T06:00:00Z',
+    number: '2029659970',
+    duration: 7,
+    outcome: 'BUSY',
+    human: false,
+  },
 ];
