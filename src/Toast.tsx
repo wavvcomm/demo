3ea6d66c19aefee56c2eb100c60638994b06d09d
Raleigh Wayland <raleigh.wayland@stormapp.com>
2021-09-22 10:01:59 -0500

    Tweaking Toast spacing

--- src/Toast.tsx
+++ src/Toast.tsx
@@ -54,7 +54,7 @@ type ToastProps = {
 const ToastContainer = styled.div<ToastProps>(({ duration }) => ({
 	position: 'absolute',
 	top: 70,
-	right: 10,
+	right: 5,
 	animation: `${fadeInOut} ${duration}s linear`,
 }));
 
